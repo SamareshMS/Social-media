d1444dd6ef78a079b8cf0a395e979a1239caa0ac
Samaresh <samareshms2002@gmail.com>
2021-11-21 16:11:03 +0530

    render page from sign up and sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile = function(req,res){
         title: "profile"
     });
 }
+
+// Render the sign up page
+module.exports.signUp = function(req,res){
+    res.render('user_sign_up', {
+        title: "Sodia | Sign Up"
+    });
+}
+
+// Render the sign in page
+module.exports.signIn = function(req,res){
+    res.render('user_sign_in', {
+        title: "Sodia | Sign In"
+    });
+}
