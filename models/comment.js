1af7d5f2393b38414f3815f2ad8622db0eea0b7d
Samaresh <samareshms2002@gmail.com>
2021-11-25 22:46:11 +0530

    add comment to the database

--- models/comment.js
+++ models/comment.js
@@ -17,6 +17,6 @@ const commentSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const Comment = mongoose.Schema('Comment', commentSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
 module.exports = Comment;
